334d6c0a9669701699603dd5a11388a2179d54b6
François Briatte <briatte@gmail.com>
2023-07-15 21:41:43 +0200

    new fixed affiliations and processing code

--- 06-query-ROR.r
+++ 06-query-ROR.r
@@ -2,7 +2,7 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
-a <- fs::dir_ls("data", regexp = "participants") %>%
+a <- fs::dir_ls("data", regexp = "epsa\\d{4}-participants") %>%
   map(readr::read_tsv, col_types = cols(.default = "c")) %>%
   map_dfr(select, affiliation) %>%
   filter(!is.na(affiliation)) %>%
@@ -70,11 +70,10 @@ for (i in nrow(d):1) {
 d <- mutate(d, ror_results = map_lgl(ror, is.character)) %>%
   filter(ror_results) %>%
   mutate(
-    ror_results = map_chr(ror,
-            ~ jsonlite::fromJSON(.x, flatten = TRUE) %>%
-              magrittr::extract2("number_of_results")
-    ),
-    ror_results = as.integer(ror_results)
+    # [NOTE] we used to `map_chr` here, and then `as.integer` the result, but
+    # it now seems safe to expect an integer straight away
+    ror_results = map_int(ror, ~ jsonlite::fromJSON(.x, flatten = TRUE) %>%
+                            magrittr::extract2("number_of_results"))
   )
 
 # export full results -----------------------------------------------------
@@ -97,6 +96,7 @@ d <- readr::read_rds(f)
 
 cat("\nExporting overview of", nrow(d), "results on disk...")
 
+# results overview
 r <- map_dfr(d$ror[ d$ror_results > 0 ],
   ~ jsonlite::fromJSON(.x, flatten = TRUE) %>%
     magrittr::extract2("items") %>%
@@ -109,15 +109,33 @@ r <- map_dfr(d$ror[ d$ror_results > 0 ],
     )
 )
 
-# queried, successfully
-bind_cols(select(filter(d, ror_results > 0), -ror), r) %>%
-  # queried, not successfully
+# ror-corrections (Google Sheets): RAW sheet = TSV below
+f <- "data/ror-corrections.tsv"
+
+# affiliations: queried, successfully
+d <- bind_cols(select(filter(d, ror_results > 0), -ror), r) %>%
+  # add queried, not successfully
   bind_rows(select(filter(d, ror_results == 0), -ror)) %>%
-  # not queried yet
-  full_join(a, ., by = "affiliation") %>%
-  # stable order
-  arrange(affiliation) %>%
-  readr::write_tsv("data/ror-overview.tsv", na = "")
+  # add not queried yet
+  full_join(a, ., by = "affiliation")
+
+if (!fs::file_exists(f)) {
+
+  # initialize file
+  arrange(d, affiliation) %>%
+    mutate(rowid = row_number(), .before = 1) %>%
+    readr::write_tsv(f, na = "")
+
+} else {
+
+  # new rows to add at end of Google Sheets
+  anti_join(arrange(d, affiliation), read_tsv(f)) %>%
+    bind_rows(read_tsv(f), .) %>%
+    # excessively safe append of additional row numbers
+    mutate(rowid = if_else(is.na(rowid), row_number(), rowid)) %>%
+    readr::write_tsv(f, na = "")
+
+}
 
 cat(" done.\n")
 
--- 07-fix-affiliations.r
+++ 07-fix-affiliations.r
@@ -1,35 +1,42 @@
 library(tidyverse)
 library(readxl)
 
-a <- "data/ror-corrections.xlsx" %>%
-  readxl::read_excel(sheet = "CORRECTED") %>%
+a <- readxl::read_excel("data/ror-corrections.xlsx", sheet = "CORRECTED") %>%
+  mutate(rowid = as.integer(rowid))
+
+# sanity checks
+stopifnot(!is.na(a$rowid))
+stopifnot(!is.na(a$keep))
+stopifnot(a$keep %in% c("c", "m", "s", "y")) # non-problematic cases
+stopifnot(!is.na(a$affiliation))
+stopifnot(!duplicated(a$affiliation))
+stopifnot(!(a$keep == "c" & is.na(a$c_name)))
+stopifnot(!(a$keep == "c" & is.na(a$c_id)))
+stopifnot(!(a$keep == "m" & is.na(a$m1_name)))
+stopifnot(!(a$keep == "m" & is.na(a$m1_id)))
+stopifnot(!(a$keep == "m" & is.na(a$m2_name)))
+stopifnot(!(a$keep == "m" & is.na(a$m2_id)))
+# 24 special values
+select(filter(a, keep == "s"), rowid, starts_with("c_")) %>% print(n = Inf)
+
+a <- a %>%
   # process corrected rows
   mutate(
     organization.name = case_when(
-      keep %in% "y" ~ organization.name,
-      keep %in% "c" ~ c_name,
-      # TODO
-      # currently using 1st affil only
-      keep %in% "m" ~ m1_name,
-      keep %in% "n" ~ NA_character_,
-      # covers `p`
-      TRUE ~ NA_character_
+      keep == "y" ~ organization.name,
+      keep == "c" ~ c_name,
+      # TODO: currently using 1st affil. only, fix?
+      keep == "m" ~ m1_name,
+      keep == "s" & c_name == "(independent)" ~ "(independent)",
+      keep == "s" & c_name != "(independent)" ~ c_name
     ),
     organization.id = case_when(
-      keep %in% "y" ~ organization.id,
-      keep %in% "c" ~ c_id,
-      # TODO
-      # currently using 1st affil only
-      keep %in% "m" ~ m1_id,
-      keep %in% "n" ~ NA_character_,
-      # covers `p`
-      TRUE ~ NA_character_
-    ) #,
-    # # countries for correctly identified affiliations
-    # country = case_when(
-    #   keep %in% "y" ~ organization.country.country_name,
-    #   TRUE ~ NA_character_
-    # )
+      keep == "y" ~ organization.id,
+      keep == "c" ~ c_id,
+      # TODO: currently using 1st affil. only, fix?
+      keep == "m" ~ m1_id,
+      keep == "s" ~ c_id # contains countries for special values
+    )
   ) %>%
   select(
     affiliation,
@@ -37,45 +44,56 @@ a <- "data/ror-corrections.xlsx" %>%
     #        deliberately do not have a ROR URL; use this instead of the name
     #        provided by the ROR data dump (problem: those participants will not
     #        have a country value)
-    affiliation_ror = organization.name,
-    affiliation_url = organization.id
+    affiliation_ror = organization.name, #
+    affiliation_url = organization.id # missing for a few special cases
   )
 
-# sanity check
-stopifnot(!duplicated(a$affiliation))
-
-# some affiliations are not assigned to a ROR entity
+# sanity check: some affiliations are not assigned to a ROR entity, but in that
+# case, we at least have their corrected country stored
 table(is.na(a$affiliation_url))
+table(str_detect(a$affiliation_url, "ror.org")) # 24 special cases
 
 # add country information -------------------------------------------------
 
-f <- "data/ror-countries.tsv"
+b <- readr::read_csv("data/v1.25-2023-05-11-ror-data.csv.zip") %>%
+  mutate(country.country_code = if_else(
+    is.na(country.country_code) & country.country_name == "Namibia", "NA",
+    country.country_code)) %>%
+  select(id, starts_with("country"))
 
-# initialize country dataset, using ROR data dump
-if (!fs::file_exists(f)) {
+a <- left_join(a, b, by = c("affiliation_url" = "id"))
 
-  b <- fs::dir_ls("data", regexp = "ror-data\\.json$")
-  if (length(b) != 1) {
-    stop("No ROR data dump found in `data` folder.")
-  }
+# sanity check: no ROR-identified affiliation has a missing country code
+stopifnot(!(str_detect(a$affiliation_url, "ror.org") &
+              is.na(a$country.country_code)))
 
-  # extract relevant columns from JSON object
-  # [NOTE] very slow (5 minutes)
-  jsonlite::fromJSON(b, flatten = TRUE) %>%
-  select(id, starts_with("country")) %>%
-    readr::write_tsv(f)
-
-}
-
-b <- readr::read_tsv(f, col_types = "ccc")
-
-# export ------------------------------------------------------------------
-
-left_join(d, b, by = c("affiliation_url" = "id")) %>%
+# for the few remaining cases, we go through a different merge
+a <- bind_rows(
+  filter(a, !is.na(country.country_code)),
+  left_join(
+    filter(a, is.na(country.country_code)) %>%
+      select(-starts_with("country")) %>%
+      mutate(country.country_name = affiliation_url,
+             affiliation_url = NA_character_)
+    ,
+    distinct(select(b, starts_with("country.")))
+    ,
+    by = "country.country_name"
+  )
+) %>%
   rename(
     affiliation_ccode = country.country_code,
     affiliation_cname = country.country_name
   ) %>%
-  write_tsv("data/ror-affiliations.tsv")
+  arrange(affiliation)
+
+# sanity checks: no missing affiliations or country codes
+stopifnot(!is.na(a$affiliation))
+stopifnot(!is.na(a$affiliation_ccode))
+stopifnot(!is.na(a$affiliation_cname))
+
+# export ------------------------------------------------------------------
+
+write_tsv(a, "data/ror-affiliations.tsv")
 
 # kthxbye
